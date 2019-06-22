e71484ad38b273bfb7d1b627cc473ba9ab12ab1e
ChapelR <Mason_718@hotmail.com>
2019-06-22 01:50:41 -0400

    Create state.js
    TODO
    
    1. get the IFID from Harlowe... I can pull this from the data-chunk.
    
    2. Port IFID keys to user prefs.
    
    3. Test Harlowe version (can pull from data chunk) to determine if session restoration is needed.
    
    4. Serialize the menu API callbacks somehow to preserve their state as well.

---
+++ src/state.js
@@ -0,0 +1,72 @@
+(function () {
+    'use strict';
+
+    // save the state to reload it on restarts...
+    // load script just before setup.js
+
+    var _key = '{ifid}_hal_restart_'; // need to get the story IFID for this
+    var _store = function () {
+        if (window.sessionStorage) {
+            save = function (key, data) {
+                window.sessionStorage.setItem(_key + 'key', data);
+            };
+            load = function (key) {
+                return window.sessionStorage.getItem(_key + 'key');
+            };
+        } else {
+            save = function () { /* no op */ };
+            load = function () { /* no op */ };
+            console.warn('Session storage is unavailable...');
+        }
+        return {
+            save : save,
+            load : load
+        };
+    };
+
+    function saveTracks () {
+        var data;
+        try {
+            data = Chapel.Audio.classes.Track.list.map( function (track) {
+                return {
+                    id : track.id,
+                    sources : track.sources
+                };
+            });
+            data = JSON.stringify(data);
+            _store.save('tracks', data);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    function loadTracks () {
+        // reprocess all track data
+        var data;
+        try {
+            data = _store.load('tracks');
+            if (data) {
+                data = JSON.parse(data);
+            }
+            if (Array.isArray(data) && data.length) {
+                data.forEach( function (def) {
+                    if (def.id && def.sources && !Chapel.Audio.classes.Track.has(def.id)) {
+                        Chapel.Audio.newTrack(def.id, def.sources);
+                    } else {
+                        console.warn('Track reload failed...');
+                    }
+                });
+            }
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    $(document).one('unload', function () {
+        // save tracks to session storage
+        saveTracks();
+    });
+
+    // reload any tracks in session storage on start-up
+    loadTracks();
+}());
