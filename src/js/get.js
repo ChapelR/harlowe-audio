bbeedd1241960d8475b5c6dc7b5a74d583c518fe
ChapelR <Mason_718@hotmail.com>
2019-07-17 00:36:28 -0400

    Update get.js

--- src/js/get.js
+++ src/js/get.js
@@ -128,16 +128,6 @@
         return $dataChunk.attr('ifid');
     }
 
-    window.Chapel = window.Chapel || {};
-
-    window.Chapel.Get = {
-        version : getHarloweVersion(),
-        isHarlowe3OrLater : version3OrLater(),
-        storyTitle : getStoryTitle(),
-        IFID : getStoryIFID(),
-        fromPassage : tracksFromPassage
-    };
-
     if (Chapel.Get.version < 2) {
         throw new Error('The Harlowe Audio Library is only designed to work with Harlowe 2 and 3; you appear to be using Harlowe 1 or an otherwise invalid story format.');
     }
@@ -168,8 +158,18 @@
         });
     }
 
+    window.Chapel = window.Chapel || {};
+
     window.Chapel.Macros = {
         add : addMacros
+    };
+
+    window.Chapel.Get = {
+        version : getHarloweVersion(),
+        isHarlowe3OrLater : version3OrLater(),
+        storyTitle : getStoryTitle(),
+        IFID : getStoryIFID(),
+        fromPassage : tracksFromPassage
     };
     
 }());
